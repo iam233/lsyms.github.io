61c2b6323cd9e4a8f3afa8d239a3becdbeb30b76
YSLMS <47308563+YSLMS@users.noreply.github.com>
2019-08-23 18:53:16 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+var request = (function (){
+    var obj = {};
+    var arr = window.location.search.slice(1).split("&");
+    for (var i = 0, len = arr.length; i < len; i++) {
+        var nv = arr[i].split("=");
+        obj[decodeURIComponent(nv[0]).toLowerCase()] = decodeURIComponent(nv[1]);
+    }
+    return obj;   
+})();
+ 
+location.href = request.page;
