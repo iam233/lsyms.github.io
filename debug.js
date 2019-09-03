1a6c5a3f73ea6d00fe69a8dd4ad578d9667aff29
YSLMS <47308563+YSLMS@users.noreply.github.com>
2019-09-03 18:28:53 +0800

    Create debug.js

---
+++ debug.js
@@ -0,0 +1,8 @@
+console.info("a chage world’s coder.");
+console.info("let codeing the world.");
+console.info("if you need help email at LSYMS@VIP.QQ.COM.");
+console.info("build code is by LSYMS.");
+console.info("by lsyms.");
+console.info("a chage world’s coder.");
+console.info("Hello,world.");
+console.info("Goodbye,afder you");
