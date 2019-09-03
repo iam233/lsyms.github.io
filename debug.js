d4c0112d47446259c9dd087c11523ee395790de0
YSLMS <47308563+YSLMS@users.noreply.github.com>
2019-09-03 18:31:04 +0800

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,8 +1,3 @@
-console.info("a chage world’s coder.");
-console.info("let codeing the world.");
 console.info("if you need help email at LSYMS@VIP.QQ.COM.");
 console.info("build code is by LSYMS.");
 console.info("by lsyms.");
-console.info("a chage world’s coder.");
-console.info("Hello,world.");
-console.info("Goodbye,afder you");
