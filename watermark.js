1f0002d56eecfc73a8e47d7634b708f0df883e85
LSYMS <58099470+LSYMS@users.noreply.github.com>
2020-01-28 10:41:49 +0800

    Update watermark.js

--- watermark.js
+++ watermark.js
@@ -33,7 +33,7 @@
     watermark_fontsize:'18px',          //水印字体大小
     watermark_alpha:0.15,               //水印透明度，要求设置在大于等于0.005
     watermark_width:100,                //水印宽度
-    watermark_height:100,               //水印长度
+    watermark_height:150,               //水印长度
     watermark_angle:15,                 //水印倾斜度数
     watermark_parent_width:0,      //水印的总体宽度（默认值：body的scrollWidth和clientWidth的较大值）
     watermark_parent_height:0,     //水印的总体高度（默认值：body的scrollHeight和clientHeight的较大值）
